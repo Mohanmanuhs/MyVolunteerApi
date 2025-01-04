c4442917decf3c41c1eaf6a9e9d4b527a15470ef
Mohanmanuhs <mohanmanuhs9@gmail.com>
2025-01-04 19:06:38 +0530

    added controllers

--- src/main/java/com/example/MyVolunteer_api/controller/UserController.java
+++ src/main/java/com/example/MyVolunteer_api/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.MyVolunteer_api.controller;
 
+import com.example.MyVolunteer_api.model.User;
 import com.example.MyVolunteer_api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -11,6 +13,26 @@ public class UserController {
     private UserService userService;
 
 
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PutMapping("/edit")
+    public User editUser(@RequestBody User user) {
+        return userService.update(user);
+    }
+
+    @PutMapping("/UpdatePassword")
+    public String updatePassword(@RequestBody User user, String newPassword) {
+        userService.changePassword(user, newPassword);
+        return "Password updated successfully";
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteUser(@RequestBody User user) {
+        return ResponseEntity.ok("User deleted successfully");
+    }
 
 
 }
